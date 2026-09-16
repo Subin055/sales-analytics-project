e36a69d51c28089d5c169aace2ef917078f40c7a
Subin055 <127193387+Subin055@users.noreply.github.com>
2026-09-16 15:53:24 +0530

    Update sales_analysis.py

--- python/sales_analysis.py
+++ python/sales_analysis.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 
 # Load the sales dataset
@@ -6,3 +5,12 @@ data = pd.read_csv("../data/sales_data.csv")
 
 # Display the first 5 rows
 print(data.head())
+
+# Calculate total sales
+total_sales = data["Sales"].sum()
+
+# Calculate total profit
+total_profit = data["Profit"].sum()
+
+print("Total Sales:", total_sales)
+print("Total Profit:", total_profit)
